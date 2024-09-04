f2635da365f435d6345d5586e0f10a99454e24ff
Eric Posante <ep270122@ohio.edu>
2024-09-04 16:36:05 -0400

    Commiting program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Eric Posante
+ *        Date: 9/4/24
+ * Description: Test
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
